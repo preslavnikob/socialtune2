322d68a44c04a040c6fd9528d3d2d579be906c05
preslavnikob <presko300@gmail.com>
2017-05-14 20:25:33 +0300

    Update settings.py

--- socialtune/settings.py
+++ socialtune/settings.py
@@ -125,3 +125,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = '/opt/socialtune/socialtune/socialtune/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/opt/socialtune/socialtune/socialtune/static/',
+]
